1177d738480b51c5ed1ed20668bd44a8b5c5f821
Eric Mertens <emertens@gmail.com>
2023-06-19 14:59:21 -0700

    Fix indentation

--- src/Toml/Value.hs
+++ src/Toml/Value.hs
@@ -19,30 +19,30 @@ import Data.Time (Day, LocalTime, TimeOfDay, ZonedTime(zonedTimeToLocalTime, zon
 
 -- | Semantic TOML value with all table assignments resolved.
 data Value
-  = Integer Integer
-  | Float Double
-  | Array [Value]
-  | Table (Map String Value)
-  | Bool Bool
-  | String String
-  | TimeOfDay TimeOfDay
-  | ZonedTime ZonedTime
-  | LocalTime LocalTime
-  | Day Day
-  deriving (Show, Read)
+    = Integer Integer
+    | Float Double
+    | Array [Value]
+    | Table (Map String Value)
+    | Bool Bool
+    | String String
+    | TimeOfDay TimeOfDay
+    | ZonedTime ZonedTime
+    | LocalTime LocalTime
+    | Day Day
+    deriving (Show, Read)
 
 instance Eq Value where
-  Integer   x == Integer   y = x == y
-  Float     x == Float     y = x == y
-  Array     x == Array     y = x == y
-  Table     x == Table     y = x == y
-  Bool      x == Bool      y = x == y
-  String    x == String    y = x == y
-  TimeOfDay x == TimeOfDay y = x == y
-  LocalTime x == LocalTime y = x == y
-  Day       x == Day       y = x == y
-  ZonedTime x == ZonedTime y = projectZT x == projectZT y
-  _           == _           = False
+    Integer   x == Integer   y = x == y
+    Float     x == Float     y = x == y
+    Array     x == Array     y = x == y
+    Table     x == Table     y = x == y
+    Bool      x == Bool      y = x == y
+    String    x == String    y = x == y
+    TimeOfDay x == TimeOfDay y = x == y
+    LocalTime x == LocalTime y = x == y
+    Day       x == Day       y = x == y
+    ZonedTime x == ZonedTime y = projectZT x == projectZT y
+    _           == _           = False
 
 -- Extract the relevant parts to build an Eq instance
 projectZT :: ZonedTime -> (LocalTime, Int)
